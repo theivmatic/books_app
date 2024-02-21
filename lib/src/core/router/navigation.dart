fd21734f2593ac371c5a1d14d8b8d2edb048a216
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-21 12:11:40 +0500

    created navigation screen

---
+++ lib/src/core/router/navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
