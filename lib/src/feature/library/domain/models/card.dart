337e85c8981adbea6357ea58add8a3c26cefb622
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-23 15:52:59 +0500

    created copy, toJson and fromJson methods for Card model

--- lib/src/feature/library/domain/models/card.dart
+++ lib/src/feature/library/domain/models/card.dart
@@ -1,3 +1,21 @@
+const String tableCards = 'cards';
+
+class CardFields {
+  static final List<String> values = [
+    id,
+    imagePath,
+    title,
+    author,
+    genre,
+    publishedYear,
+    pagesQuantity,
+    description,
+    status,
+    pageBookmark,
+    comment,
+  ];
+}
+
 class Card {
   final int? id;
   final String? imagePath;
@@ -5,7 +23,7 @@ class Card {
   final String? author;
   final String? genre;
   final int? publishedYear;
-  final String? pagesQuantity;
+  final int? pagesQuantity;
   final String? description;
   final String? status;
   final int? pageBookmark;
@@ -24,4 +42,62 @@ class Card {
     required this.pageBookmark,
     required this.comment,
   });
+
+  Card copy({
+   int? id;
+   String? imagePath;
+   String? title;
+   String? author;
+   String? genre;
+   int? publishedYear;
+   int? pagesQuantity;
+   String? description;
+   String? status;
+   int? pageBookmark;
+   String? comment;
+}) => 
+      Card(id: id ?? this.id, 
+      imagePath: imagePath ?? this.imagePath, 
+      title: title ?? this.title, 
+      author: author ?? this.author, 
+      genre: genre ?? this.genre, 
+      publishedYear: publishedYear ?? this.publishedYear, 
+      pagesQuantity: pagesQuantity ?? this.pagesQuantity, 
+      description: description ?? this.description, 
+      status: status ?? this.status, 
+      pageBookmark: pageBookmark ?? this.pageBookmark, 
+      comment: comment ?? this.comment,
+      );
+
+      static Card fromJson(Map<String, Object?> json) => Card(
+        id: json[CardFields.id] as int?, 
+        imagePath: json[CardFields.imagePath] as String, 
+        title: json[CardFields.title] as String, 
+        author: json[CardFields.author] as String, 
+        genre: json[CardFields.genre] as String, 
+        publishedYear: json[CardFields.publishedYear] as int, 
+        pagesQuantity: json[CardFields.pagesQuantity] as int, 
+        description: json[CardFields.description] as String, 
+        status: json[CardFields.status] as String, 
+        pageBookmark: json[CardFields.pageBookmark] as int, 
+        comment: json[CardFields.comment] as String,
+        );
+
+        Map<String, Object?> toJson() => {
+          CardFields.id: id,
+          CardFields.imagePath: imagePath,
+          CardFields.title: title,
+          CardFields.author: author,
+          CardFields.genre: genre,
+          CardFields.publishedYear: publishedYear,
+          CardFields.pagesQuantity: pagesQuantity,
+          CardFields.description: description,
+          CardFields.status: status,
+          CardFields.pageBookmark: pageBookmark,
+          CardFields.comment: comment,
+        };
 }
+
+
+
+      
