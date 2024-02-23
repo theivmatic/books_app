fc45251d283ad9c6e136713aa0fd7a175d84069c
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-23 16:45:37 +0500

    made appbar without searchbar for opened books screen

--- lib/src/feature/opened_books/presentation/screens/opened_books.dart
+++ lib/src/feature/opened_books/presentation/screens/opened_books.dart
@@ -1,11 +1,43 @@
+import 'package:books_app/src/core/constants/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OpenedBooksScreen extends StatelessWidget {
   const OpenedBooksScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Открытые книги'),
+        titleSpacing: 20.w,
+        titleTextStyle: TextStyles.appbarTitleText,
+        centerTitle: false,
+        elevation: 5,
+        shadowColor: AppColors.elevButtnShadow,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            20,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: FloatingActionButton(
+              onPressed: () {},
+              elevation: 0,
+              shape: const CircleBorder(),
+              backgroundColor: AppColors.yellow,
+              child: Icon(
+                Icons.add,
+                color: AppColors.white,
+                size: 28.dg,
+              ),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: AppColors.backgroundColor,
       body: Center(
         child: Text('Opened books'),
       ),
