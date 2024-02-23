cba0f155ced41f9b285a4e5535a15dd908b6d069
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-23 16:17:58 +0500

    created states for card bloc

--- lib/src/feature/library/domain/bloc/card_state.dart
+++ lib/src/feature/library/domain/bloc/card_state.dart
@@ -1,6 +1,35 @@
 part of 'card_bloc.dart';
 
 @immutable
-sealed class CardBlocState {}
+sealed class CardBlocState extends Equatable {
+  const CardBlocState();
+}
 
-final class CardBlocInitialState extends CardBlocState {}
+final class CardBlocInitialState extends CardBlocState {
+  @override
+  List<Object?> get props => [];
+}
+
+class DisplayCards extends CardBlocState {
+  final List<Card> card;
+  final String imagePath;
+
+  const DisplayCards({
+    required this.card,
+    required this.imagePath,
+  });
+
+  @override
+  List<Object> get props => [card];
+}
+
+class DisplaySpecificCards extends CardBlocState {
+  final Card card;
+
+  const DisplaySpecificCards({
+    required this.card,
+  });
+
+  @override
+  List<Object> get props => [card];
+}
