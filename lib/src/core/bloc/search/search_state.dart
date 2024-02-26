7f5c54a09f1ebf020758b26122acbd7f95b56f93
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-26 20:54:26 +0500

    created search bloc states

--- lib/src/core/bloc/search/search_state.dart
+++ lib/src/core/bloc/search/search_state.dart
@@ -3,3 +3,7 @@ part of 'search_bloc.dart';
 sealed class SearchBlocState {}
 
 final class SearchBlocInitialState extends SearchBlocState {}
+
+final class SearchBlocLoadingState extends SearchBlocState {}
+
+final class SearchBlocLoadedState extends SearchBlocState {}
