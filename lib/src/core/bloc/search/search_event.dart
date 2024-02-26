11750a972587add18b654187edfa3b212eac5704
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-26 20:55:37 +0500

    created search bloc event

--- lib/src/core/bloc/search/search_event.dart
+++ lib/src/core/bloc/search/search_event.dart
@@ -1,3 +1,11 @@
 part of 'search_bloc.dart';
 
 sealed class SearchBlocEvent {}
+
+class SearchWord extends SearchBlocEvent {
+  final String word;
+
+  SearchWord({
+    required this.word,
+  });
+}
