ce96b0b553dd1f97a632bfaf507089d9b4c7f194
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-27 19:01:40 +0500

    added elevation to card widget on opened book screen

--- lib/src/feature/opened_books/presentation/widgets/opened_book.dart
+++ lib/src/feature/opened_books/presentation/widgets/opened_book.dart
@@ -32,89 +32,92 @@ class _OpenedBookWidgetState extends State<OpenedBookWidget> {
       padding: EdgeInsets.all(
         20.dg,
       ),
-      child: InkWell(
-        onTap: () {},
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              width: 350.w,
-              height: 190.h,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.r),
-              ),
-              child: Image.asset(
-                'assets/images/upload_image.png',
-                scale: 0.5,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                padding: EdgeInsets.all(10.dg),
+      child: Material(
+        elevation: 5,
+        child: InkWell(
+          onTap: () {},
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
                 width: 350.w,
-                height: 95.h,
+                height: 190.h,
                 decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(
-                      20.r,
-                    ),
-                    bottomRight: Radius.circular(
-                      20.r,
+                  borderRadius: BorderRadius.circular(20.r),
+                ),
+                child: Image.asset(
+                  'assets/images/upload_image.png',
+                  scale: 0.5,
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  padding: EdgeInsets.all(10.dg),
+                  width: 350.w,
+                  height: 95.h,
+                  decoration: BoxDecoration(
+                    color: AppColors.white,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(
+                        20.r,
+                      ),
+                      bottomRight: Radius.circular(
+                        20.r,
+                      ),
                     ),
                   ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.card.author ?? '',
-                          style: TextStyles.labelText,
-                        ),
-                        Text(
-                          widget.card.title ?? '',
-                          style: TextStyles.settingTileText,
-                        ),
-                        const Expanded(
-                          child: SizedBox(),
-                        ),
-                        Text(
-                          'Закладка на странице:',
-                          style: TextStyles.labelText,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          counter,
-                          style: TextStyles.labelText.copyWith(
-                            color: AppColors.yellow,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.card.author ?? '',
+                            style: TextStyles.labelText,
                           ),
-                        ),
-                        const Expanded(
-                          child: SizedBox(),
-                        ),
-                        Text(widget.card.pageBookmark ?? '0'),
-                      ],
-                    ),
-                  ],
+                          Text(
+                            widget.card.title ?? '',
+                            style: TextStyles.settingTileText,
+                          ),
+                          const Expanded(
+                            child: SizedBox(),
+                          ),
+                          Text(
+                            'Закладка на странице:',
+                            style: TextStyles.labelText,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            counter,
+                            style: TextStyles.labelText.copyWith(
+                              color: AppColors.yellow,
+                            ),
+                          ),
+                          const Expanded(
+                            child: SizedBox(),
+                          ),
+                          Text(widget.card.pageBookmark ?? '0'),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            LinearProgressBar(
-              progressColor: AppColors.yellow,
-              backgroundColor: AppColors.grey,
-              maxSteps: 100,
-              currentStep: percent,
-              minHeight: 2.h,
-            ),
-          ],
+              LinearProgressBar(
+                progressColor: AppColors.yellow,
+                backgroundColor: AppColors.grey,
+                maxSteps: 100,
+                currentStep: percent,
+                minHeight: 2.h,
+              ),
+            ],
+          ),
         ),
       ),
     );
