0042e8d30bed238ad6382987bb6a60b6abab246d
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-02-29 17:03:09 +0500

    added search to OpenedBooksScreen

--- lib/src/feature/opened_books/presentation/screens/opened_books.dart
+++ lib/src/feature/opened_books/presentation/screens/opened_books.dart
@@ -1,7 +1,9 @@
 import 'package:books_app/src/core/constants/app_theme.dart';
 import 'package:books_app/src/core/widgets/custom_appbar.dart';
+import 'package:books_app/src/feature/library/data/cards_db.dart';
 // import 'package:books_app/src/core/widgets/search_bar.dart';
 import 'package:books_app/src/feature/library/domain/bloc/card_bloc.dart';
+import 'package:books_app/src/feature/library/domain/models/card.dart';
 import 'package:books_app/src/feature/library/presentation/screens/add_card.dart';
 import 'package:books_app/src/feature/library/presentation/widgets/card.dart';
 import 'package:books_app/src/feature/opened_books/presentation/widgets/opened_book.dart';
@@ -18,7 +20,27 @@ class OpenedBooksScreen extends StatefulWidget {
 }
 
 class _OpenedBooksScreenState extends State<OpenedBooksScreen> {
+  //RadioButton
   int? selectedValue;
+
+  //Search
+  final searchController = TextEditingController();
+  final databaseHelper = CardsDatabase.instance;
+  final String userSearchInput = '';
+  bool doItJustOnce = false;
+  List<BookCard> list = [];
+  List<BookCard> filteredList = <BookCard>[];
+  void filterList(String value) {
+    setState(() {
+      filteredList = list
+          .where(
+            (text) => text.title!.toLowerCase().contains(
+                  value.toLowerCase(),
+                ),
+          )
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -106,7 +128,8 @@ class _OpenedBooksScreenState extends State<OpenedBooksScreen> {
                                               child: Row(
                                                 children: [
                                                   Radio(
-                                                    activeColor: AppColors.yellow,
+                                                    activeColor:
+                                                        AppColors.yellow,
                                                     value: state
                                                         .bookCard[index].id,
                                                     groupValue: selectedValue,
@@ -178,13 +201,96 @@ class _OpenedBooksScreenState extends State<OpenedBooksScreen> {
                 ),
               );
             } else {
-              return ListView.builder(
-                itemCount: state.bookCard.length,
-                itemBuilder: (context, index) {
-                  return OpenedBookWidget(
-                    card: state.bookCard[index],
-                  );
-                },
+              return Stack(
+                children: [
+                  ListView.builder(
+                    itemCount: state.bookCard.length,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return Column(
+                          children: [
+                            SizedBox(
+                              height: 65.h,
+                            ),
+                            OpenedBookWidget(
+                              card: state.bookCard[index],
+                            ),
+                          ],
+                        );
+                      }
+                      return OpenedBookWidget(
+                        card: state.bookCard[index],
+                      );
+                    },
+                  ),
+                  Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: AppColors.white,
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20.r),
+                              bottomRight: Radius.circular(20.r),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(20.w, 0, 20.w, 20.h),
+                            child: TextFormField(
+                              style: TextStyles.inputText,
+                              controller: searchController,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: AppColors.searchBackgroundColor,
+                                prefixIcon: const Icon(
+                                  Icons.search,
+                                ),
+                                prefixIconColor: AppColors.searchIconColor,
+                                hintText: 'Поиск',
+                                hintStyle: TextStyles.bottomButtonText
+                                    .copyWith(color: AppColors.searchIconColor),
+                                border: InputBorder.none,
+                              ),
+                              onChanged: filterList,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.yellow,
+                              borderRadius: BorderRadius.circular(20.r),
+                            ),
+                            child: FutureBuilder(
+                              future: databaseHelper.readAllCards(),
+                              builder: (
+                                context,
+                                snapshot,
+                              ) {
+                                if (searchController.text.isNotEmpty) {
+                                  if (!doItJustOnce) {
+                                    list = snapshot.data!;
+                                    filteredList = list;
+                                    doItJustOnce = !doItJustOnce;
+                                  }
+                                  return ListView.builder(
+                                    itemCount: filteredList.length,
+                                    itemBuilder: (context, index) {
+                                      return OpenedBookWidget(
+                                        card: filteredList[index],
+                                      );
+                                    },
+                                  );
+                                }
+                                return const SizedBox();
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
               );
             }
           }
