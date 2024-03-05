830e65b1e1e7be2656350bbe027858baae45fff0
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-03-05 16:46:46 +0500

    added image under app bar to ArticleDetailsScreen, created ArticleDetailWidget

--- lib/src/feature/about_books/presentation/screens/article_details.dart
+++ lib/src/feature/about_books/presentation/screens/article_details.dart
@@ -1,5 +1,6 @@
 import 'package:books_app/src/core/constants/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ArticleDetailsScreen extends StatelessWidget {
   const ArticleDetailsScreen({super.key});
@@ -22,6 +23,9 @@ class ArticleDetailsScreen extends StatelessWidget {
           ],
         ),
         centerTitle: false,
+        elevation: 0,
+        surfaceTintColor: AppColors.white,
+        backgroundColor: AppColors.white,
         leading: IconButton(
           onPressed: () {
             Navigator.of(context).pop();
@@ -30,6 +34,50 @@ class ArticleDetailsScreen extends StatelessWidget {
             Icons.arrow_back_ios,
             color: AppColors.yellow,
           ),
+        ),
+      ),
+      backgroundColor: AppColors.backgroundColor,
+      body: SingleChildScrollView(
+        physics: const ClampingScrollPhysics(),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20.r),
+                  bottomRight: Radius.circular(20.r),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(20.dg),
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    width: 350.w,
+                    height: 200.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.r),
+                    ),
+                    child: Image.asset(
+                      'assets/images/upload_image.png',
+                      scale: 0.5,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.r),
+                color: AppColors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/src/feature/about_books/presentation/widgets/article_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArticleDetailWidget extends StatelessWidget {
+  const ArticleDetailWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
