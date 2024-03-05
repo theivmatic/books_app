13616d384d52a6e286da9dba3a8bc7d0e3b222c2
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-03-05 08:15:58 +0500

    added variables and methods for search and storage

--- lib/src/feature/will_read/presentation/screens/will_read.dart
+++ lib/src/feature/will_read/presentation/screens/will_read.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:books_app/src/core/constants/app_theme.dart';
 import 'package:books_app/src/core/widgets/custom_appbar.dart';
+import 'package:books_app/src/feature/library/data/cards_db.dart';
 import 'package:books_app/src/feature/library/domain/bloc/card_bloc.dart';
 import 'package:books_app/src/feature/library/domain/models/card.dart';
 import 'package:books_app/src/feature/library/presentation/screens/add_card.dart';
@@ -8,6 +11,7 @@ import 'package:cupertino_modal_sheet/cupertino_modal_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WillReadScreen extends StatefulWidget {
   const WillReadScreen({super.key});
@@ -20,8 +24,65 @@ class _WillReadScreenState extends State<WillReadScreen> {
   //RadioButton
   int? selectedValue;
 
+  //Search
+  final searchController = TextEditingController();
+  final databaseHelper = CardsDatabase.instance;
+  final String userSearchInput = '';
+  bool doItJustOnce = false;
+  List<BookCard> list = [];
+  List<BookCard> filteredList = <BookCard>[];
+  void filterList(String value) {
+    setState(() {
+      filteredList = list
+          .where(
+            (text) => text.title!.toLowerCase().contains(
+                  value.toLowerCase(),
+                ),
+          )
+          .toList();
+    });
+  }
+
   //Strorage
   List<BookCard> selectedBooks = [];
+
+  late SharedPreferences storage;
+
+  Future<dynamic> getSharedPreferences() async {
+    storage = await SharedPreferences.getInstance();
+    readFromStorage();
+    setState(() {});
+  }
+
+  void saveToStorage() {
+    final bookCardStringList =
+        selectedBooks.map((bookCard) => jsonEncode(bookCard.toJson())).toList();
+    storage.setStringList('BookCards', bookCardStringList);
+  }
+
+  void readFromStorage() {
+    final bookCardStringList = storage.getStringList('BookCards');
+
+    if (bookCardStringList != null) {
+      selectedBooks = bookCardStringList
+          .map(
+            (bookCard) => BookCard.fromJson(
+              json.decode(bookCard) as Map<String, Object?>,
+            ),
+          )
+          .toList();
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getSharedPreferences();
+    // context.read<OpenedBookBloc>().add(
+    //       const DisplayOpenedBooks(),
+    //     );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
