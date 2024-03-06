aae5ad02fd63ad6717ae11e55c89fc338ba6d68d
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-03-06 13:01:58 +0500

    added json_annotation package to pubspec

--- lib/src/feature/about_books/domain/models/article_entity.dart
+++ lib/src/feature/about_books/domain/models/article_entity.dart
@@ -1,9 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
 class ArticleEntity {
 	List<Article>? articles;
 
 	ArticleEntity({this.articles});
 }
 
+@JsonSerializable()
 class Article {
 	String? title;
 	String? imagePath;
@@ -12,6 +16,7 @@ class Article {
 	Article({this.title, this.imagePath, this.items});
 }
 
+@JsonSerializable()
 class Item {
 	String? title;
   String? text;
