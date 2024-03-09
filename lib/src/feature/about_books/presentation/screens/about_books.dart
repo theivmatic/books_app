7cdfca83014f25a30170a7c8ebab3483c0d03eaf
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-03-09 10:41:20 +0500

    deleted floating action button from AboutBooksScreen appBar

--- lib/src/feature/about_books/presentation/screens/about_books.dart
+++ lib/src/feature/about_books/presentation/screens/about_books.dart
@@ -1,5 +1,4 @@
 import 'package:books_app/src/core/constants/app_theme.dart';
-import 'package:books_app/src/core/widgets/custom_appbar.dart';
 import 'package:books_app/src/feature/about_books/domain/bloc/about_books_bloc.dart';
 import 'package:books_app/src/feature/about_books/presentation/widgets/article.dart';
 import 'package:flutter/material.dart';
@@ -25,9 +24,16 @@ class _AboutBooksScreenState extends State<AboutBooksScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        title: 'О книгах',
-        onPressed: () {},
+      appBar: AppBar(
+        title: const Text(
+          'О книгах',
+        ),
+        titleTextStyle: TextStyles.appbarTitleText,
+        centerTitle: false,
+        elevation: 0,
+        shadowColor: AppColors.elevButtnShadow,
+        surfaceTintColor: AppColors.white,
+        backgroundColor: AppColors.white,
       ),
       backgroundColor: AppColors.backgroundColor,
       body: BlocBuilder<AboutBooksBloc, AboutBooksBlocState>(
@@ -43,14 +49,16 @@ class _AboutBooksScreenState extends State<AboutBooksScreen> {
                       ),
                       ArticleWidget(
                         article: state.articlesLoaded,
-                        title: state.articlesLoaded.articles?[index].title ?? 'Название статьи',
+                        title: state.articlesLoaded.articles?[index].title ??
+                            'Название статьи',
                       ),
                     ],
                   );
                 } else {
                   return ArticleWidget(
                     article: state.articlesLoaded,
-                    title: state.articlesLoaded.articles?[index].title ?? 'Название статьи',
+                    title: state.articlesLoaded.articles?[index].title ??
+                        'Название статьи',
                   );
                 }
               },
